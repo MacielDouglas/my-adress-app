a4579472cc4c3f47740331355d5e734602001fa7
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-10-25 16:00:45 -0300

    add tailwind apollo client

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
+export default client;
+
+createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/graphql": {
+        target: "https://minha-api-nine.vercel.app",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
